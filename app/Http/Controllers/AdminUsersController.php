f5f95e632daa4615d520b7fbfafdb3ac67a16417
Dorin Sima <sima.dorin.octavian@gmail.com>
2020-07-15 11:13:23 +0300

    users page working withouth photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\User;
+use App\Role;
 use Illuminate\Http\Request;
 
 class AdminUsersController extends Controller
@@ -13,7 +16,8 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -23,7 +27,9 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        return view('admin.users.create');
+        $roles = Role::pluck('name', 'id')->all();
+        // dd(compact(roles));
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -32,9 +38,10 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        return view('admin.users.store');
+        User::create($request->all());
+        return redirect('admin/users');
     }
 
     /**
